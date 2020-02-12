1ae769a015d4a96c05745cf1828b8f70a794586f
ghunbotMay <ghunbot.dawit@aliznet.fr>
2020-02-12 18:20:41 +0100

    fixing some issues of mongoose

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ require('dotenv').config({ path: 'variables.env' });
 
 // Connect to our Database and handle any bad connections
 mongoose.connect(process.env.DATABASE, {
-  //useMongoClient: true :- removed it's no longer necessary to handle any bad connections instead use useNewUrlParser: true 
-  // useMongoClient: true
-  useNewUrlParser: true
+  //useMongoClient: true :- removed it's no longer necessary to handle any bad connections instead use useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
